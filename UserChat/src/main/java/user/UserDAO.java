7bba0a50b460b25099faf3e72d428538eb7d2e5f
ggambopce <ggambopce@gmail.com>
2024-02-27 23:23:14 +0900

    로그인함수생성

---
+++ UserChat/src/main/java/user/UserDAO.java
@@ -0,0 +1,56 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class UserDAO {
+
+	DataSource dataSource;
+	
+	public UserDAO() {
+		try {
+			InitialContext initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
+			dataSource = (DataSource) envContext.lookup("jdbc/UserChat");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int login(String userID, String userPassword) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String SQL = "SELECT * FROM USER WHERE userID = ?";
+		try {
+			conn = dataSource.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				if(rs.getString("userPassword").equals(userPassword)) {
+					return 1; // 로그인 성공
+				}
+				return 2; // 로그인 실패
+			} else {
+				return 0; // 해당 사용자가 존재하지 않음
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return -1; //데이터베이서 오류
+	}
+}
