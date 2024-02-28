0b0ed045a89afc137835de169de26690ba655428
ggambopce <ggambopce@gmail.com>
2024-02-28 09:35:56 +0900

    중복체크

--- UserChat/src/main/java/user/UserDAO.java
+++ UserChat/src/main/java/user/UserDAO.java
@@ -53,4 +53,68 @@ public class UserDAO {
 		}
 		return -1; //데이터베이서 오류
 	}
+	
+	public int registerCheck(String userID) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String SQL = "SELECT * FROM USER WHERE userID = ?";
+		try {
+			conn = dataSource.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			if(rs.next() || userID.equals("")) {
+				return 0; // 이미 존재하는 회원
+			} else { 
+				return 1; // 가입 가능한 회원 아이디
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return -1; //데이터베이서 오류
+	}
+	
+	public int registerCheck(String userID, String userPassword, String userName, String userAge, String userGender, String userEmail, String userProfile) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String SQL = "INSERT INTO USER VALUES (?, ?, ?, ?, ?, ?, ?)";
+		try {
+			conn = dataSource.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			pstmt.setString(2, userPassword);
+			pstmt.setString(3, userName);
+			pstmt.setInt(4, Integer.parseInt(userAge));
+			pstmt.setString(5, userGender);
+			pstmt.setString(6, userEmail);
+			pstmt.setString(7, userProfile);
+			rs = pstmt.executeQuery();
+			if(rs.next() || userID.equals("")) {
+				return 0; // 이미 존재하는 회원
+			} else { 
+				return 1; // 가입 가능한 회원 아이디
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return -1; //데이터베이서 오류
+	}
 }
